f064c082a852e6b8e999bdfc71ff757840644d2e
SaiPranay2011 <chebiumsaipranay@gmail.com>
2021-07-05 15:41:22 +0530

    gsign

--- Meble_TheFurnitureApp/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/meble_thefurnitureapp/databinding/ActivityHomePageBinding.java
+++ Meble_TheFurnitureApp/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/meble_thefurnitureapp/databinding/ActivityHomePageBinding.java
@@ -4,25 +4,41 @@ package com.example.meble_thefurnitureapp.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewbinding.ViewBinding;
 import com.example.meble_thefurnitureapp.R;
+import com.google.android.material.navigation.NavigationView;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityHomePageBinding implements ViewBinding {
   @NonNull
-  private final LinearLayout rootView;
+  private final DrawerLayout rootView;
 
-  private ActivityHomePageBinding(@NonNull LinearLayout rootView) {
+  @NonNull
+  public final AppBarHomePageBinding appBarHomePage;
+
+  @NonNull
+  public final DrawerLayout drawerLayout;
+
+  @NonNull
+  public final NavigationView navView;
+
+  private ActivityHomePageBinding(@NonNull DrawerLayout rootView,
+      @NonNull AppBarHomePageBinding appBarHomePage, @NonNull DrawerLayout drawerLayout,
+      @NonNull NavigationView navView) {
     this.rootView = rootView;
+    this.appBarHomePage = appBarHomePage;
+    this.drawerLayout = drawerLayout;
+    this.navView = navView;
   }
 
   @Override
   @NonNull
-  public LinearLayout getRoot() {
+  public DrawerLayout getRoot() {
     return rootView;
   }
 
@@ -43,10 +59,29 @@ public final class ActivityHomePageBinding implements ViewBinding {
 
   @NonNull
   public static ActivityHomePageBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.app_bar_home_page;
+      View appBarHomePage = rootView.findViewById(id);
+      if (appBarHomePage == null) {
+        break missingId;
+      }
+      AppBarHomePageBinding binding_appBarHomePage = AppBarHomePageBinding.bind(appBarHomePage);
 
-    return new ActivityHomePageBinding((LinearLayout) rootView);
+      DrawerLayout drawerLayout = (DrawerLayout) rootView;
+
+      id = R.id.nav_view;
+      NavigationView navView = rootView.findViewById(id);
+      if (navView == null) {
+        break missingId;
+      }
+
+      return new ActivityHomePageBinding((DrawerLayout) rootView, binding_appBarHomePage,
+          drawerLayout, navView);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
--- Meble_TheFurnitureApp/app/src/main/java/com/example/meble_thefurnitureapp/LoginPage.java
+++ Meble_TheFurnitureApp/app/src/main/java/com/example/meble_thefurnitureapp/LoginPage.java
@@ -11,10 +11,17 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.meble_thefurnitureapp.databinding.ActivityLoginPageBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 
 import org.jetbrains.annotations.NotNull;
@@ -23,6 +30,7 @@ public class LoginPage extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseDatabase database;
     ActivityLoginPageBinding binding;
+    GoogleSignInClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +40,46 @@ public class LoginPage extends AppCompatActivity {
             startActivity(new Intent(this,HomePage.class));
             finish();
         }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+        client = GoogleSignIn.getClient(this,gso);
+
     }
 
     public void gsign(View view) {
+        Intent i = client.getSignInIntent();
+        startActivityForResult(i,0);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            firebaseGsign(account.getIdToken(),account.getEmail());
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void firebaseGsign(String idToken, String email) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken,null);
+        auth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(LoginPage.this,HomePage.class));
+                    finish();
+                }else{
+                    Toast.makeText(LoginPage.this, "failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 
     public void register(View view) {
         Intent i = new Intent(this,RegisterPage.class);
