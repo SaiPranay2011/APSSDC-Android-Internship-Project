7444ac54ff42d78212ec19dbc19540cd99579d98
laasya2704 <60955399+laasya2704@users.noreply.github.com>
2021-07-05 11:14:01 +0530

    Added welcome page and login page

---
+++ Meble_TheFurnitureApp/app/src/main/java/com/example/meble_thefurnitureapp/LoginPage.java
@@ -0,0 +1,24 @@
+package com.example.meble_thefurnitureapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+
+    public void gsign(View view) {
+    }
+
+    public void register(View view) {
+    }
+
+    public void login(View view) {
+    }
+}
