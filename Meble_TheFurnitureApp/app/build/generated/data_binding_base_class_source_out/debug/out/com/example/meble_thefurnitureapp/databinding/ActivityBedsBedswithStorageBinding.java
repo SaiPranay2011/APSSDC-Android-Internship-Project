206ec9ef75bbed9fb97d6137ead3cef6b52f346b
SaiPranay2011 <chebiumsaipranay@gmail.com>
2021-07-14 13:32:21 +0530

    Beds Description

--- Meble_TheFurnitureApp/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/meble_thefurnitureapp/databinding/ActivityBedsBedswithStorageBinding.java
+++ Meble_TheFurnitureApp/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/meble_thefurnitureapp/databinding/ActivityBedsBedswithStorageBinding.java
@@ -5,19 +5,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ScrollView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewbinding.ViewBinding;
 import com.example.meble_thefurnitureapp.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityBedsBedswithStorageBinding implements ViewBinding {
   @NonNull
   private final ScrollView rootView;
 
-  private ActivityBedsBedswithStorageBinding(@NonNull ScrollView rootView) {
+  @NonNull
+  public final TextView rate;
+
+  @NonNull
+  public final Toolbar toolbar;
+
+  private ActivityBedsBedswithStorageBinding(@NonNull ScrollView rootView, @NonNull TextView rate,
+      @NonNull Toolbar toolbar) {
     this.rootView = rootView;
+    this.rate = rate;
+    this.toolbar = toolbar;
   }
 
   @Override
@@ -43,10 +55,25 @@ public final class ActivityBedsBedswithStorageBinding implements ViewBinding {
 
   @NonNull
   public static ActivityBedsBedswithStorageBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.rate;
+      TextView rate = rootView.findViewById(id);
+      if (rate == null) {
+        break missingId;
+      }
 
-    return new ActivityBedsBedswithStorageBinding((ScrollView) rootView);
+      id = R.id.toolbar;
+      Toolbar toolbar = rootView.findViewById(id);
+      if (toolbar == null) {
+        break missingId;
+      }
+
+      return new ActivityBedsBedswithStorageBinding((ScrollView) rootView, rate, toolbar);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
