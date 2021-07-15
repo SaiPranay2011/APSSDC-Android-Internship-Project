adf8cac5353efdf5a20280901b06e4073f6d8465
laasya2704 <60955399+laasya2704@users.noreply.github.com>
2021-07-15 17:48:35 +0530

    TV & Media Furniture payment updated

--- Meble_TheFurnitureApp/app/src/main/java/com/example/meble_thefurnitureapp/TVMedia_TvMediaFurniture.java
+++ Meble_TheFurnitureApp/app/src/main/java/com/example/meble_thefurnitureapp/TVMedia_TvMediaFurniture.java
@@ -3,12 +3,21 @@ package com.example.meble_thefurnitureapp;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
-public class TVMedia_TvMediaFurniture extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class TVMedia_TvMediaFurniture extends AppCompatActivity implements PaymentResultListener {
+
+    String API_KEY = "rzp_test_ug6aiUEmeZMscW";
+    Checkout checkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +40,310 @@ public class TVMedia_TvMediaFurniture extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    public void Buy_tv(View view) {
+
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Norwica TV Furniture Set");
+            object.put("amount","2600000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv1(View view) {
+
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Thomas TV Furniture Set");
+            object.put("amount","1600000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv2(View view) {
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Andrew TV Furniture Set ");
+            object.put("amount","1800000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv3(View view) {
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Berlin TV Furniture Set ");
+            object.put("amount","2200000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv4(View view) {
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Megllan TV Furniture Set");
+            object.put("amount","2500000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv5(View view) {
+
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Robust Zebra Print TV Furniture");
+            object.put("amount","2200000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv6(View view) {
+
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Jonas Wooden TV Furniture Set");
+            object.put("amount","2700000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv7(View view) {
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Yugo TV Furniture Set");
+            object.put("amount","1700000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv8(View view) {
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Arata TV Furniture Set");
+            object.put("amount","1900000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Buy_tv9(View view) {
+        checkout = new Checkout();
+        checkout.setKeyID(API_KEY);
+
+        final Activity activity = this;
+
+        try {
+            JSONObject object = new JSONObject();
+            object.put("Name","Oscar TV Furniture Set");
+            object.put("amount","2050000");
+            object.put("theme.color","#44BB04");
+            object.put("currency","INR");
+            JSONObject retryObj = new JSONObject();
+            retryObj.put("enabled", true);
+            retryObj.put("max_count", 4);
+            object.put("retry", retryObj);
+
+            checkout.open(activity,object);
+
+
+
+        }
+
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
     }
 }
